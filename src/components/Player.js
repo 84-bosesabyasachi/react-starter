761c748a14bcee64b2be78855c7269a7ff008a21
84-bosesabyasachi <sabyasachibose1602@gmail.com>
2026-01-04 01:53:25 +0530

    update

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,27 +1,44 @@
-import React,{useRef} from 'react';
+import React,{use, useRef, useState} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faPlay,faAngleLeft, faAngleRight} from '@fortawesome/free-solid-svg-icons';
 
-const Player = ({currentSong})=> {
+const Player = ({currentSong, isPlaying, setIsPlaying})=> {
+    
     //refs
     const audioRef = useRef(null);
     //event handlers
     const playSongHandler = ()=> {
-        console.log(audioRef.current);
+        if(isPlaying){
+            audioRef.current.pause();
+            setIsPlaying(!isPlaying);
+        } else{
+            audioRef.current.play();
+            setIsPlaying(!isPlaying); 
+        }
     };
+    const timeUpdateHandler = (e)=> {
+        const current = e.target.currentTime;
+        const duration = e.target.duration;
+        setSongInfo({...songInfo, currentTime: current, duration});
+    };
+    //state
+    const [songInfo, setSongInfo] = useState({
+        currentTime: null,
+        duration: null,
+    });
     return(
         <div className= "Player">
         <div className="time-control">
-            <p>Start type </p>
+            <p>{songInfo.currentTime}</p>
             <input type="range" />
-            <p>End time</p>
+            <p>{songInfo.duration}</p>
         </div>
         <div className="play-control">
             <FontAwesomeIcon className="skip-back" size="2x"  icon={faAngleLeft} />
             <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} />
             <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
             </div>
-            <audio ref={audioRef} src={currentSong.audio}></audio>
+            <audio onTimeUpdate={timeUpdateHandler} ref={audioRef} src={currentSong.audio}></audio>
         </div>
     );
 };
--- src/util.js
+++ src/util.js
@@ -1,64 +1,70 @@
-import { v4 as uuidv4 } from 'uuid';
-function chillHop(){
-    return [
-        {
-            name: "DEMOTAPES_90_songstarter_tapeloop__piano_Emaj.wav",
-            cover: "https://splice-res.cloudinary.com/image/upload/f_auto,q_auto,w_auto/c_limit,w_450/v1764014047/hxad525z7lxwn6mqrjpb.jpg",
-            artist: "demotapes - volume 3 (sunset)",   
-            audio: "https://splice.com/sounds/sample/5b5b8dfefc1f2a823b5197a18a289e04fdd1da6e06ecfe1dd6b364b3706ad8ce",
-            color: ["#205950", "#2ab3bf"],
-            id: uuidv4(),
-            active: true,
-        },
-        {
-            name: "DS_VAH3_122_keys_piano_chords_maye_Bbmin.wav",
-            cover: "https://splice-res.cloudinary.com/image/upload/f_auto,q_auto,w_auto/c_limit,w_450/v1755629826/tsgnihwcbeex5rfqejuv.jpg",
-            artist: "Vocal Afro House 3",
-            audio: "https://splice.com/sounds/sample/32c015468a6b9f91ea1a86df1d3381ae05fab0abf85c16d3702ea3da8ab7223c",
-            color: ["#205950", "#2ab3bf"],
-            id: uuidv4(),
-            active: false,
-        },
+import { v4 as uuidv4 } from "uuid";
 
-        {
-            name: "OS_AD_95_piano_chords_hieroglyphics_Am.wav",
-            cover: "https://splice-res.cloudinary.com/image/upload/f_auto,q_auto,w_auto/c_limit,w_450/v1736973556/y0btdkcgmetcyswsumoh.jpg",
-            artist: "adore. - rnb soul",
-            audio: "https://splice.com/sounds/sample/8f29f11bcaf07c8310d57631766c1bea73447da0e7ba35771265f4ae51313eaa",
-            color: ["#205950", "#2ab3bf"],
-            id: uuidv4(),
-            active: false,
-        },
-        { 
-            name:"keep OS_VV3_70_piano_chords_nectar_Fm.wav",
-            cover:"https://splice-res.cloudinary.com/image/upload/f_auto,q_auto,w_auto/c_limit,w_450/v1733980374/jdhtzgzwpgzcs3u2ifd8.jpg",
-            artist:"VINTAGE VIBES 3",
-            audio:"https://splice.com/sounds/sample/600d26c31ce2c964233f65e50c260eab470cbb79fc9a885422530005c21957d5",
-            color:["#EF8EA9", "#ab417f"],
-            id: uuidv4(),
-            active: false,
-        },
-        {
-            name: "OS_6ix_92_piano_chords_wrongside_A#m.wav",
-            cover: "https://splice-res.cloudinary.com/image/upload/q_auto/f_auto/c_fit,h_100/dpr_1.0/v1761924341/lgv581gdx33ogmsmgczx.jpg",
-            artist: "6ixsense - toronto r&b",
-            audio: "https://splice.com/sounds/sample/3ffc6700de24b5cab81d65e0617a87fca18306543d099090ae60beae96302c77",
-            color: ["#E5D9D9", "#E5D9D9"],
-            id: uuidv4(),
-            active: false,
-        },
-        {
-            name:"OS_AFR2_110_songstarter_jara_Cm.wav",
-            cover:"https://splice-res.cloudinary.com/image/upload/q_auto/f_auto/c_fit,w_544/dpr_1.0/v1698251653/okcb2bznfbbircycmmsa.jpg",
-            artist:"amapiano",
-            audio:"https://splice.com/sounds/sample/213defd77f23d5a3ea7659fb2a392664457395b098f260edb75ed0cca48a19c7",
-            color:["#4E496C", "#B3B1C6"],
-            id: uuidv4(),
-            active: false,
-        },
+function chillHop() {
+  return [
+    {
+      name: "No Copyright Music Lofi",
+      cover: "https://cdn.pixabay.com/audio/2025/04/20/19-17-03-210_200x200.jpg",
+      artist: "lkoliks",
+      audio: "/assets/lofi.mp3", // ✅ IMPORTANT
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+  ];
+}
+
+export default chillHop;
+
+        // {
+        //     name: "DS_VAH3_122_keys_piano_chords_maye_Bbmin.wav",
+        //     cover: "https://splice-res.cloudinary.com/image/upload/f_auto,q_auto,w_auto/c_limit,w_450/v1755629826/tsgnihwcbeex5rfqejuv.jpg",
+        //     artist: "Vocal Afro House 3",
+        //     audio: "https://splice.com/sounds/sample/32c015468a6b9f91ea1a86df1d3381ae05fab0abf85c16d3702ea3da8ab7223c",
+        //     color: ["#205950", "#2ab3bf"],
+        //     id: uuidv4(),
+        //     active: false,
+        // },
+
+        // {
+        //     name: "OS_AD_95_piano_chords_hieroglyphics_Am.wav",
+        //     cover: "https://splice-res.cloudinary.com/image/upload/f_auto,q_auto,w_auto/c_limit,w_450/v1736973556/y0btdkcgmetcyswsumoh.jpg",
+        //     artist: "adore. - rnb soul",
+        //     audio: "https://splice.com/sounds/sample/8f29f11bcaf07c8310d57631766c1bea73447da0e7ba35771265f4ae51313eaa",
+        //     color: ["#205950", "#2ab3bf"],
+        //     id: uuidv4(),
+        //     active: false,
+        // },
+        // { 
+        //     name:"keep OS_VV3_70_piano_chords_nectar_Fm.wav",
+        //     cover:"https://splice-res.cloudinary.com/image/upload/f_auto,q_auto,w_auto/c_limit,w_450/v1733980374/jdhtzgzwpgzcs3u2ifd8.jpg",
+        //     artist:"VINTAGE VIBES 3",
+        //     audio:"https://splice.com/sounds/sample/600d26c31ce2c964233f65e50c260eab470cbb79fc9a885422530005c21957d5",
+        //     color:["#EF8EA9", "#ab417f"],
+        //     id: uuidv4(),
+        //     active: false,
+        // },
+        // {
+        //     name: "OS_6ix_92_piano_chords_wrongside_A#m.wav",
+        //     cover: "https://splice-res.cloudinary.com/image/upload/q_auto/f_auto/c_fit,h_100/dpr_1.0/v1761924341/lgv581gdx33ogmsmgczx.jpg",
+        //     artist: "6ixsense - toronto r&b",
+        //     audio: "https://splice.com/sounds/sample/3ffc6700de24b5cab81d65e0617a87fca18306543d099090ae60beae96302c77",
+        //     color: ["#E5D9D9", "#E5D9D9"],
+        //     id: uuidv4(),
+        //     active: false,
+        // },
+        // {
+        //     name:"OS_AFR2_110_songstarter_jara_Cm.wav",
+        //     cover:"https://splice-res.cloudinary.com/image/upload/q_auto/f_auto/c_fit,w_544/dpr_1.0/v1698251653/okcb2bznfbbircycmmsa.jpg",
+        //     artist:"amapiano",
+        //     audio:"https://splice.com/sounds/sample/213defd77f23d5a3ea7659fb2a392664457395b098f260edb75ed0cca48a19c7",
+        //     color:["#4E496C", "#B3B1C6"],
+        //     id: uuidv4(),
+        //     active: false,
+        // },
         
     
 
-    ];
-}
-export default chillHop;
+//     ];
+// }
+// export default chillHop;
