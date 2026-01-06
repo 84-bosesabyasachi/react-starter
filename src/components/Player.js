2dccd738ab38e37b178af2f261168c657df80530
84-bosesabyasachi <sabyasachibose1602@gmail.com>
2026-01-07 00:31:38 +0530

    update

--- src/components/Player.js
+++ src/components/Player.js
@@ -21,6 +21,15 @@ const Player = ({currentSong, isPlaying, setIsPlaying})=> {
         const duration = e.target.duration;
         setSongInfo({...songInfo, currentTime: current, duration});
     };
+    const getTime = (time)=> {
+        return(
+            Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+        );
+    }
+    const dragHandler = (e)=> {
+        audioRef.current.currentTime = e.target.value;
+        setSongInfo({...songInfo, currentTime: e.target.value});
+    };
     //state
     const [songInfo, setSongInfo] = useState({
         currentTime: null,
@@ -29,16 +38,25 @@ const Player = ({currentSong, isPlaying, setIsPlaying})=> {
     return(
         <div className= "Player">
         <div className="time-control">
-            <p>{songInfo.currentTime}</p>
-            <input type="range" />
-            <p>{songInfo.duration}</p>
+            <p>{getTime(songInfo.currentTime)}</p>
+            <input
+             min={0} 
+             max = {songInfo.duration || 0} 
+             value={songInfo.currentTime || 0}
+             type="range" 
+                onChange={dragHandler}
+            />
+            <p>{getTime(songInfo.duration)}</p>
         </div>
         <div className="play-control">
             <FontAwesomeIcon className="skip-back" size="2x"  icon={faAngleLeft} />
             <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} />
             <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
             </div>
-            <audio onTimeUpdate={timeUpdateHandler} ref={audioRef} src={currentSong.audio}></audio>
+            <audio
+            onTimeUpdate={timeUpdateHandler} 
+            onLoadedMetadata={timeUpdateHandler}
+            ref={audioRef} src={currentSong.audio}></audio>
         </div>
     );
 };
--- src/util.js
+++ src/util.js
@@ -6,65 +6,60 @@ function chillHop() {
       name: "No Copyright Music Lofi",
       cover: "https://cdn.pixabay.com/audio/2025/04/20/19-17-03-210_200x200.jpg",
       artist: "lkoliks",
-      audio: "/assets/lofi.mp3", // ✅ IMPORTANT
+      audio: "/assets/lofi.mp3", 
       color: ["#205950", "#2ab3bf"],
       id: uuidv4(),
       active: true,
     },
+    {
+            name: "Soft House",
+            cover: "https://cdn.pixabay.com/audio/2025/12/28/10-43-20-309_200x200.jpg",
+            artist: "Nature_Music",
+            audio: "/assets/lofi-1.mp3",
+            color: ["#205950", "#2ab3bf"],
+            id: uuidv4(),
+            active: false,
+        },
+
+        {
+            name: "Gardens - Stylish Chill",
+            cover: "https://cdn.pixabay.com/audio/2025/02/19/22-25-20-336_200x200.png",
+            artist: "penguinmusic",
+            audio: "/assets/lofi-2.mp3",
+            color: ["#205950", "#2ab3bf"],
+            id: uuidv4(),
+            active: false,
+        },
+        { 
+            name:"Background Music Upbeat",
+            cover:"https://cdn.pixabay.com/audio/2025/09/16/07-21-47-846_200x200.png",
+            artist:"DELOSound",
+            audio:"/assets/lofi-3.mp3",
+            color:["#EF8EA9", "#ab417f"],
+            id: uuidv4(),
+            active: false,
+        },
+        {
+            name: "New Beginnings",
+            cover: "https://cdn.pixabay.com/audio/2022/10/06/03-04-33-598_200x200.jpg",
+            artist: "BeauWalker",
+            audio: "/assets/lofi-4.mp3",
+            color: ["#E5D9D9", "#E5D9D9"],
+            id: uuidv4(),
+            active: false,
+        },
+        {
+            name:"20 Galaxies_piano edit",
+            cover:"https://cdn.pixabay.com/audio/2022/12/06/20-07-16-764_200x200.png",
+            artist:"JMidiBytes",
+            audio:"/assets/lofi-5.mp3",
+            color:["#4E496C", "#B3B1C6"],
+            id: uuidv4(),
+            active: false,
+        },
   ];
 }
 
 export default chillHop;
 
-        // {
-        //     name: "DS_VAH3_122_keys_piano_chords_maye_Bbmin.wav",
-        //     cover: "https://splice-res.cloudinary.com/image/upload/f_auto,q_auto,w_auto/c_limit,w_450/v1755629826/tsgnihwcbeex5rfqejuv.jpg",
-        //     artist: "Vocal Afro House 3",
-        //     audio: "https://splice.com/sounds/sample/32c015468a6b9f91ea1a86df1d3381ae05fab0abf85c16d3702ea3da8ab7223c",
-        //     color: ["#205950", "#2ab3bf"],
-        //     id: uuidv4(),
-        //     active: false,
-        // },
-
-        // {
-        //     name: "OS_AD_95_piano_chords_hieroglyphics_Am.wav",
-        //     cover: "https://splice-res.cloudinary.com/image/upload/f_auto,q_auto,w_auto/c_limit,w_450/v1736973556/y0btdkcgmetcyswsumoh.jpg",
-        //     artist: "adore. - rnb soul",
-        //     audio: "https://splice.com/sounds/sample/8f29f11bcaf07c8310d57631766c1bea73447da0e7ba35771265f4ae51313eaa",
-        //     color: ["#205950", "#2ab3bf"],
-        //     id: uuidv4(),
-        //     active: false,
-        // },
-        // { 
-        //     name:"keep OS_VV3_70_piano_chords_nectar_Fm.wav",
-        //     cover:"https://splice-res.cloudinary.com/image/upload/f_auto,q_auto,w_auto/c_limit,w_450/v1733980374/jdhtzgzwpgzcs3u2ifd8.jpg",
-        //     artist:"VINTAGE VIBES 3",
-        //     audio:"https://splice.com/sounds/sample/600d26c31ce2c964233f65e50c260eab470cbb79fc9a885422530005c21957d5",
-        //     color:["#EF8EA9", "#ab417f"],
-        //     id: uuidv4(),
-        //     active: false,
-        // },
-        // {
-        //     name: "OS_6ix_92_piano_chords_wrongside_A#m.wav",
-        //     cover: "https://splice-res.cloudinary.com/image/upload/q_auto/f_auto/c_fit,h_100/dpr_1.0/v1761924341/lgv581gdx33ogmsmgczx.jpg",
-        //     artist: "6ixsense - toronto r&b",
-        //     audio: "https://splice.com/sounds/sample/3ffc6700de24b5cab81d65e0617a87fca18306543d099090ae60beae96302c77",
-        //     color: ["#E5D9D9", "#E5D9D9"],
-        //     id: uuidv4(),
-        //     active: false,
-        // },
-        // {
-        //     name:"OS_AFR2_110_songstarter_jara_Cm.wav",
-        //     cover:"https://splice-res.cloudinary.com/image/upload/q_auto/f_auto/c_fit,w_544/dpr_1.0/v1698251653/okcb2bznfbbircycmmsa.jpg",
-        //     artist:"amapiano",
-        //     audio:"https://splice.com/sounds/sample/213defd77f23d5a3ea7659fb2a392664457395b098f260edb75ed0cca48a19c7",
-        //     color:["#4E496C", "#B3B1C6"],
-        //     id: uuidv4(),
-        //     active: false,
-        // },
         
-    
-
-//     ];
-// }
-// export default chillHop;
